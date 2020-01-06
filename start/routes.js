6b4c37e3983d875317238350ff3cc70f0bde5683
Remulo Costa <remulo.costa@gmail.com>
2020-01-06 03:41:34 -0300

    criado recuperação de senha do usuário

--- start/routes.js
+++ start/routes.js
@@ -3,9 +3,7 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-// Route.get('/', () => {
-//   return { greeting: 'Hello world in JSON' }
-// })
-
 Route.post('users', 'UserController.store')
 Route.post('sessions', 'SessionController.store')
+
+Route.post('passwords', 'ForgotPasswordController.store')
