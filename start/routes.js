571a8c7b53175e2701a52b9a6a00b2a83a9b318a
Remulo Costa <remulo.costa@gmail.com>
2020-01-07 03:15:29 -0300

    validação das rotas

--- start/routes.js
+++ start/routes.js
@@ -3,11 +3,15 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.post('users', 'UserController.store')
-Route.post('sessions', 'SessionController.store')
+Route.post('users', 'UserController.store').validator('User')
+Route.post('sessions', 'SessionController.store').validator('Session')
 
-Route.post('passwords', 'ForgotPasswordController.store')
-Route.put('passwords', 'ForgotPasswordController.update')
+Route.post('passwords', 'ForgotPasswordController.store').validator(
+  'ForgotPassword'
+)
+Route.put('passwords', 'ForgotPasswordController.update').validator(
+  'ResetPassword'
+)
 
 Route.get('/files/:id', 'FileController.show')
 
@@ -15,7 +19,20 @@ Route.group(() => {
   Route.post('/files', 'FileController.store')
 
   // Route.get('/projects', 'ProjectController.index')
-  Route.resource('projects', 'ProjectController').apiOnly()
+  Route.resource('projects', 'ProjectController')
+    .apiOnly()
+    .validator(
+      new Map([
+        [['projects.store'], ['Project']] //,
+        // [['projects.store'], ['Project']] //,
+        // [
+        //   ['projects.update'],
+        //   ['ProjectUp']
+        // ]
+      ])
+    )
 
-  Route.resource('projects.tasks', 'TaskController').apiOnly()
+  Route.resource('projects.tasks', 'TaskController')
+    .apiOnly()
+    .validator(new Map([[['projects.tasks.store'], ['Task']]]))
 }).middleware(['auth'])
